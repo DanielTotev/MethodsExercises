c61545a71d5d46f2375da61a2226c1f01f2120ca
DanielTotev <dtotev1@gmail.com>
2017-06-10 11:52:26 +0300

    Added a sloved problem

---
+++ FibbonacciNumbers/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿namespace FibbonacciNumbers
+{
+    using System;
+
+    public class Fibonacci
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintFibonacci(n);
+        }
+        public static void PrintFibonacci(int n)
+        {
+            var f0 = 1;
+            var f1 = 1;
+            for (var i = 0; i < n - 1; i++)
+            {
+                var fNext = f0 + f1;
+                f0 = f1;
+                f1 = fNext;
+            }
+            Console.WriteLine(f1);
+        }
+    }
+}
